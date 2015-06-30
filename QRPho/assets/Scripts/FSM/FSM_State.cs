2c4c1aae24e479893163ac1e12aadea6dd637066
Koutacles <111412@student.sae.edu.au>
2015-06-30 11:41:32 +1000

    FSM added

---
+++ QRPho/assets/Scripts/FSM/FSM_State.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+//T represents the owner of the FSM
+public abstract class FSM_State<T> {
+
+	//When the state is first run
+	abstract public void Begin(T obj);
+
+	//When the state runs
+	abstract public void Run(T obj);
+
+	//When the state has control returned to it
+	abstract public void Resume(T obj);
+
+	//When the state relinquishes control to the new top state
+	abstract public void Pause(T obj);
+
+	//Before the state is removed from the stack
+	abstract public void End(T obj);
+}
