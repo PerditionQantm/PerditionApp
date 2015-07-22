318c250a19791fa00f1d7828aa453b35f563a70d
Kouta <kouta@koutacles.com.au>
2015-07-22 14:13:45 +1000

    +Phase test now includes a hard-coded map that can be moved around locally

---
+++ QRPho/assets/Scripts/GameBoard/Tile.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	public GameObject goUpLink;
+	public GameObject goDownLink;
+	public GameObject goLeftLink;
+	public GameObject goRightLink;
+}
