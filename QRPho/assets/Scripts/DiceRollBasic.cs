598088d5a2926543f014be7a21fecb5d7254274a
Kouta <kouta@koutacles.com.au>
2015-07-29 09:14:05 +1000

    Dice roll basic

---
+++ QRPho/assets/Scripts/DiceRollBasic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.UI;
+
+public class DiceRollBasic : MonoBehaviour {
+
+	public AudioSource asDiceRoll;
+
+	public Text txtOutput;
+
+	List<int> l_iDice;
+	public int iDiceAmount = 6;
+
+	// Use this for initialization
+	void Start () {
+		l_iDice = new List<int>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		txtOutput.text = "Results: ";
+		foreach (int die in l_iDice) {
+			txtOutput.text += " " + die.ToString();
+		}
+	}
+
+
+}
