8ce469d1df97bfa9edfbcdf165a4a84c2c55dedb
XatuXach <jakmundane@gmail.com>
2015-07-28 21:22:33 +1000

    Network Changes/Infrastructure

---
+++ QRPho/assets/Scripts/Network/LobbyManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class LobbyManager : NetworkLobbyManager
+{
+		//public NetworkLobbyManager m_NetMng;
+		public string m_ssLobbyName = "";
+		public string m_ssLocalAddress = "";
+		
+
+		void Start ()
+		{
+				
+				m_ssLocalAddress = Network.player.ipAddress;
+				this.networkAddress = m_ssLocalAddress;
+		}
+
+		public void StartLobby ()
+		{
+				if (m_ssLobbyName == "") {
+						return;
+				}
+				base.StartHost ();
+		}
+
+		public void setLobbyName ()
+		{
+				InputField temp = UIPanelManager.getUIElementOnPanel ("HostName").GetComponent<InputField> ();
+				if (temp != null) {
+						m_ssLobbyName = temp.text;
+				} else {
+						Debug.Log ("No Element named HostName on this Panel");
+				}
+				return;			
+		}
+
+		void Update ()
+		{
+//
+		}
+
+		public override void OnClientConnect (NetworkConnection conn)
+		{
+				Debug.Log ("Player Connected");
+				base.OnClientConnect (conn);
+		}
+
+}
